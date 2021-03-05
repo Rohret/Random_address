1f9480494d7f5cea85d7fd06ec7a1d22b6aae506
adamrohr <rohret@hotmail.se>
2021-03-05 17:45:58 +0100

    Typ allt funkar

--- ran.c
+++ ran.c
@@ -1,9 +1,50 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-int shuffle(int *ran_address, int *deny, int k, int *adress)
+
+struct Node
+{
+   int address;
+   struct Node *next;
+};
+
+void printlist(struct Node *head)
 {
 
+   while (head != NULL)
+   {
+      printf("Linked list: %d ", head->address);
+      head = head->next;
+   }
+}
+
+void add_new_node(struct Node **head, int address)
+{
+
+   struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+   struct Node *last = *head;
+
+   new_node->address = address;
+   new_node->next = NULL;
+
+   if (*head == NULL)
+   {
+
+      *head = new_node;
+      return;
+   }
+
+   while (last->next != NULL)
+   {
+      last = last->next;
+   }
+   last->next = new_node;
+
+   return;
+}
+
+int shuffle(int *deny, int k, int *adress, struct Node **head)
+{
    int r = ((rand() % 6));
    for (int i = 0; i < 6; i++)
    {
@@ -13,23 +54,20 @@ int shuffle(int *ran_address, int *deny, int k, int *adress)
       }
    }
 
-   if (*(adress + k) != 0)
+   if (*(adress + r) != 0)
    {
-      *(ran_address + r) = *(adress + k);
-      *(deny + k) = r;
+
+      add_new_node(head, *(adress + r));
    }
+   *(deny + k) = r;
+
    k++;
    return k;
 }
 
-void print_arr(int *address, int *deny)
+void print_arr(int *deny)
 {
 
-   for (int i = 0; i < 6; i++)
-   {
-
-      printf("Random Lista: %d ", *(address + i));
-   }
    printf("\n");
    for (int i = 0; i < 6; i++)
    {
@@ -41,22 +79,25 @@ void print_arr(int *address, int *deny)
 
 int main()
 {
+   struct Node *head = NULL;
    int k = 0;
    int curr_k = 0;
    srand(time(NULL)); // Initialization, should only be called once.
-   int arr_address[] = {10, 20, 0, 0, 50, 60};
-   int ran_arr_address[6];
+   int arr_address[] = {10, 20, 30, 40, 50, 60};
+   int ran_arr_address[6]; //Måsta ha kvar den annars funkar hänger det sig när jag kör programmet
    int deny_arr[6];
    for (int i = 0; i < 6; i++)
    {
+
       curr_k = k;
-      k = shuffle(ran_arr_address, deny_arr, k, arr_address);
+      k = shuffle(deny_arr, k, arr_address, &head);
       if (curr_k == k)
       {
          i--;
       }
    }
 
-   print_arr(ran_arr_address, deny_arr);
+   //print_arr(deny_arr);
+   printlist(head);
    return 0;
 }
